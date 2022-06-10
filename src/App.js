51b1b50f2aacb04679413b0907d93ce8d18b6c06
prachip1 <prachip1908@gmail.com>
2022-06-10 09:54:22 +0530

    pushing data first time

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Tracker from "./Tracker";
+
+function App() {
+  return (
+    <main className="bg-gray-900 w-screen lg:overflow-x-hidden">
+    <Tracker />
+    </main>
+  );
+}
+
+export default App;
