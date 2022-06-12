e9ee954396175ad92485c4074a4c8e00a13abd72
prachip1 <prachip1908@gmail.com>
2022-06-12 13:08:33 +0530

    addingappcss

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Tracker from "./Tracker";
 import MyNav from "./MyNav";
+import "./App.css";
 
 function App() {
   return (
