5b833196abd22c42419f3182cf7e5efe3ee83d46
prachip1 <prachip1908@gmail.com>
2022-06-12 12:42:56 +0530

    routerchnages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Tracker from "./Tracker";
 import MyNav from "./MyNav";
 
 function App() {
   return (
     <main className="bg-gray-900 w-screen lg:overflow-x-hidden">
+      <Router>
       <MyNav />
-    <Tracker />
+     <Tracker />
+     <Routes>
+      <Route exact path="/cryptotracker" element={<Home />} />
+     
+    </Routes>
+    </Router>
     </main>
   );
 }
